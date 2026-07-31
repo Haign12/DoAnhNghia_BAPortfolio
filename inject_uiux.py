8d76f211de42653c6f0052cb8859c8c4b795f534
Nghĩaaaa <anhnghia9a633@gmail.com>
2026-08-01 03:28:46 +0700

    Restore UI/UX projects section in main portfolio

---
+++ inject_uiux.py
@@ -0,0 +1,22 @@
+﻿import re
+
+with open('uiux-portfolio.html', 'r', encoding='utf-8') as f:
+    uiux_content = f.read()
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    idx_content = f.read()
+
+match = re.search(r'(<section class=\"projects\" id=\"uiux-projects\"[\s\S]*?</section>)', uiux_content)
+if not match:
+    print('Could not find uiux-projects in uiux-portfolio.html')
+    exit(1)
+
+uiux_section = match.group(1)
+
+if 'id=\"uiux-projects\"' in idx_content:
+    print('Already in index.html')
+else:
+    idx_content = idx_content.replace('<footer id=\"contact\">', f'{uiux_section}\n\n    <footer id=\"contact\">')
+    with open('index.html', 'w', encoding='utf-8') as f:
+        f.write(idx_content)
+    print('Injected successfully!')
